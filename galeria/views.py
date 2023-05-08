718df6f2ce264f6aa29606013873419abaa98648
Arthur Oliveira <arthurolivsira@gmail.com>
2023-05-08 20:08:34 -0300

    Adicionando legenas e nome das imagens através do DJANGO template

--- galeria/views.py
+++ galeria/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"nome": "Nebulosa de Carina", 
+        "legenda": "webbtelescope.org / NASA / James Web"},
+    2: {"nome": "Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+}
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem (request):
     return render(request, 'galeria/imagem.html')
